f3c1891f8c476f0b2d22f54610ab09425ef83734
ssshpaklevka <apactop2004@gmail.com>
2025-02-04 19:32:39 +0700

    fix

--- src/widgets/slider/slider.tsx
+++ src/widgets/slider/slider.tsx
@@ -126,7 +126,7 @@ const Slider: FC = () => {
 
   const VideoWrapper = ({
     src,
-    isActive, // Добавляем новый проп
+    isActive,
     textSrc,
   }: {
     index: number;
@@ -137,19 +137,25 @@ const Slider: FC = () => {
     const videoRef = useRef<HTMLVideoElement>(null);
 
     useEffect(() => {
-      if (videoRef.current) {
+      const videoElement = videoRef.current;
+      if (videoElement) {
+        // Предзагрузка видео
+        videoElement.preload = 'auto';
+
         if (isActive) {
-          videoRef.current.play().catch((error) => {
-            if (error.name !== 'AbortError') {
+          // Используем более надежный метод воспроизведения
+          const playPromise = videoElement.play();
+          if (playPromise !== undefined) {
+            playPromise.catch((error) => {
               return error;
-            }
-          });
+            });
+          }
         } else {
-          videoRef.current.pause();
-          videoRef.current.currentTime = 0;
+          videoElement.pause();
+          videoElement.currentTime = 0;
         }
       }
-    }, [isActive]); // Теперь зависим только от isActive
+    }, [isActive]);
 
     return (
       <motion.div
@@ -164,12 +170,13 @@ const Slider: FC = () => {
           ref={videoRef}
           playsInline
           muted
-          loop
+          preload="auto" // Добавлен атрибут preload
           className="w-[210px] h-[329px] sm:w-[350px] sm:h-[550px] md:w-[350px] md:h-[550px] lg:w-[474px] lg:h-[744px] xl:w-[589px] xl:h-[925px] 2xl:w-[460px] 2xl:h-[720px] 3xl:w-[574px] 3xl:h-[900px] object-cover"
         >
-          <source src={src.mov} type="video/quicktime" />
+          {/* Порядок источников важен для совместимости */}
           <source src={src.mp4} type="video/mp4" />
           <source src={src.webm} type="video/webm" />
+          <source src={src.mov} type="video/quicktime" />
         </video>
 
         {isActive && (
@@ -180,10 +187,12 @@ const Slider: FC = () => {
             className="absolute inset-0 mb-[30px] flex items-end justify-center"
           >
             <Image
-              alt="fef"
+              alt="Brand logo"
               width={200}
               height={90}
               src={textSrc}
+              // Добавьте loading="lazy" для отложенной загрузки
+              loading="lazy"
               className="w-[110px] h-[50px] sm:w-[200px] sm:h-[40px] lg:w-[272px] lg:h-[60px] xl:w-[338px] xl:h-[80px] 2xl:w-[200px] 2xl:h-[40px] 3xl:w-[250px] 3xl:h-[60px]"
             />
           </motion.div>
